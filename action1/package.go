9d720367d9729620a3383143060134700e498c4b
advanced-go <mark.wilson.go@outlook.com>
2024-08-11 07:03:48 -0500

    Update package.go

--- action1/package.go
+++ action1/package.go
@@ -6,13 +6,22 @@ import (
 )
 
 const (
-	PkgPath           = "github/advanced-go/experience/action1"
-	inferenceResource = "inference"
+	PkgPath = "github/advanced-go/experience/action1"
 )
+
+// GetRateLimiting - get latest rate limiting action
+func GetRateLimiting(ctx context.Context, origin core.Origin) (*RateLimiting, *core.Status) {
+	return &RateLimiting{Limit: -1, Burst: -1}, core.StatusOK()
+}
 
 // AddRateLimiting - insert rate limiting action
 func AddRateLimiting(ctx context.Context, origin core.Origin, inferenceId int, limit float64, burst int) *core.Status {
 	return core.StatusOK()
+}
+
+// GetRouting - get latest routing action
+func GetRouting(ctx context.Context, origin core.Origin) (*Routing, *core.Status) {
+	return &Routing{Location: "", Percentage: -1}, core.StatusOK()
 }
 
 // AddRouting - insert routing action
