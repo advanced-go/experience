a0f2e0a40d7008fdcbe67c8c96fea9532562779b
advanced-go <mark.wilson.go@outlook.com>
2024-08-11 05:47:55 -0500

    Updating action

--- action1/entry.go
+++ action1/entry.go
@@ -1,11 +1,6 @@
 package action1
 
 import (
-	"errors"
-	"fmt"
-	"github.com/advanced-go/experience/common"
-	"github.com/advanced-go/stdlib/core"
-	"net/url"
 	"time"
 )
 
@@ -31,98 +26,20 @@ const (
 )
 
 var (
-	safeEntry = common.NewSafe()
+	//safeEntry = common.NewSafe()
 	entryData = []Entry{
-		{Region: "us-west1", Zone: "a", Host: "www.host1.com", AgentId: "agent-id", RouteName: "host", Details: "information", Action: "processed", CreatedTS: time.Date(2024, 6, 10, 7, 120, 35, 0, time.UTC)},
-		{Region: "us-west1", Zone: "a", Host: "www.host2.com", AgentId: "agent-id", RouteName: "host", Details: "text", Action: "processed", CreatedTS: time.Date(2024, 6, 10, 7, 120, 35, 0, time.UTC)},
+		{Region: "us-west1", Zone: "a", Host: "www.host1.com", AgentId: "agent-id", CreatedTS: time.Date(2024, 6, 10, 7, 120, 35, 0, time.UTC)},
+		{Region: "us-west1", Zone: "a", Host: "www.host2.com", AgentId: "agent-id", CreatedTS: time.Date(2024, 6, 10, 7, 120, 35, 0, time.UTC)},
 	}
 )
 
 // Entry - host
 type Entry struct {
+	EntryId   int       `json:"entry-id"`
 	Region    string    `json:"region"`
 	Zone      string    `json:"zone"`
 	SubZone   string    `json:"sub-zone"`
 	Host      string    `json:"host"`
 	CreatedTS time.Time `json:"created-ts"`
-
-	AgentId   string `json:"agent-id"`
-	RouteName string `json:"route"`
-
-	// Details + action
-	Details string `json:"details"`
-	Action  string `json:"action"`
-}
-
-func (e Entry) Origin() core.Origin {
-	return core.Origin{Region: e.Region, Zone: e.Zone, SubZone: e.SubZone, Host: e.Host}
-}
-
-func (Entry) Scan(columnNames []string, values []any) (e Entry, err error) {
-	for i, name := range columnNames {
-		switch name {
-		case AgentIdName:
-			e.AgentId = values[i].(string)
-		case CreatedTSName:
-			e.CreatedTS = values[i].(time.Time)
-
-		case RegionName:
-			e.Region = values[i].(string)
-		case ZoneName:
-			e.Zone = values[i].(string)
-		case SubZoneName:
-			e.SubZone = values[i].(string)
-		case HostName:
-			e.Host = values[i].(string)
-		case RouteName:
-			e.RouteName = values[i].(string)
-
-		case ActionName:
-			e.Action = values[i].(string)
-		case DetailsName:
-			e.Details = values[i].(string)
-		default:
-			err = errors.New(fmt.Sprintf("invalid field name: %v", name))
-			return
-		}
-	}
-	return
-}
-
-func (e Entry) Values() []any {
-	return []any{
-		e.AgentId,
-		e.CreatedTS,
-
-		e.Region,
-		e.Zone,
-		e.SubZone,
-		e.Host,
-		e.RouteName,
-
-		e.Action,
-		e.Details,
-	}
-}
-
-func (Entry) Rows(entries []Entry) [][]any {
-	var values [][]any
-
-	for _, e := range entries {
-		values = append(values, e.Values())
-	}
-	return values
-}
-
-func validEntry(values url.Values, e Entry) bool {
-	if values == nil {
-		return false
-	}
-	filter := core.NewOrigin(values)
-	target := core.Origin{Region: e.Region, Zone: e.Zone, SubZone: e.SubZone, Host: e.Host}
-	if !core.OriginMatch(target, filter) {
-		return false
-	}
-	// Additional filtering
-	return true
+	AgentId   string    `json:"agent-id"`
 }
