6ab820068151c2a4c9af7fe0a12b154b36411b0b
advanced-go <mark.wilson.go@outlook.com>
2024-08-11 07:36:32 -0500

    Update package.go

--- action1/package.go
+++ action1/package.go
@@ -10,26 +10,26 @@ const (
 )
 
 // GetRateLimiting - get latest rate limiting action
-func GetRateLimiting(ctx context.Context, origin core.Origin) (*RateLimiting, *core.Status) {
-	return &RateLimiting{Limit: -1, Burst: -1}, core.StatusOK()
+func GetRateLimiting(ctx context.Context, origin core.Origin) (RateLimiting, *core.Status) {
+	return RateLimiting{Limit: -1, Burst: -1}, core.StatusOK()
 }
 
 // AddRateLimiting - insert rate limiting action
-func AddRateLimiting(ctx context.Context, origin core.Origin, inferenceId int, limit float64, burst int) *core.Status {
+func AddRateLimiting(ctx context.Context, origin core.Origin, action RateLimiting) *core.Status {
 	return core.StatusOK()
 }
 
 // GetRouting - get latest routing action
-func GetRouting(ctx context.Context, origin core.Origin) (*Routing, *core.Status) {
-	return &Routing{Location: "", Percentage: -1}, core.StatusOK()
+func GetRouting(ctx context.Context, origin core.Origin) (Routing, *core.Status) {
+	return Routing{Location: "", Percentage: -1}, core.StatusOK()
 }
 
 // AddRouting - insert routing action
-func AddRouting(ctx context.Context, origin core.Origin, inferenceId int, location string, percent int) *core.Status {
+func AddRouting(ctx context.Context, origin core.Origin, action Routing) *core.Status {
 	return core.StatusOK()
 }
 
 // AddRedirect - insert redirect action
-func AddRedirect(ctx context.Context, origin core.Origin, inferenceId int, location string, status string) *core.Status {
+func AddRedirect(ctx context.Context, origin core.Origin, action Redirect) *core.Status {
 	return core.StatusOK()
 }
